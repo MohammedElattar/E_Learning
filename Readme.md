1c2b9e331ddfef9097f5274429e3b73bf29797fa
Zeyad Farhat <zeyadfarhat145@gmail.com>
2022-11-15 23:11:47 +0200

    Setup Demo Form

--- Readme.md
+++ Readme.md
@@ -1,1 +1,17 @@
-# Raect , Laravel Project
+# E-Learning Website
+
+## To Start
+
+type in your terminal to install dependencies
+
+```
+npm install
+```
+
+then
+
+```
+npm run dev
+```
+
+and the server will start on [https://localhost:3000](https://localhost:3000)
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(`name entered =>`, name);
+
+    axios
+      .post("/api/1", name)
+      .then((response) => {
+        console.log(`Post Success: Response =>`, response);
+      })
+      .catch((error) => {
+        console.log(`fetch Failed: Error Below`);
+        console.error(error);
+      });
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
